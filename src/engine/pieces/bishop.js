03c1b12c5bd34bb56ca3f029f98c407cb61d6679
Lauren Flavell <Lauren.Flavell@softwire.com>
2021-08-25 17:57:02 +0100

    Bishop moves diagonally in any direction

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -11,19 +11,28 @@ export default class Bishop extends Piece {
     getAvailableMoves(board) {
         let location = board.findPiece(this)
         const moves = [];
-        const maxIndexNum = 7
+        const MAX_INDEX = 7
 
         if (this.player === Player.WHITE) {
-            for (let i=0, j=location.col-location.row ; j <= maxIndexNum; i++,j++) {
-            if(location.row != i && j != -1)
-                moves.push(Square.at(i, j));
-        }
-            for (let i=0, j=location.row+location.col ; i <= location.row+location.col; i++,j--) {
-            if(location.row != i)
-                moves.push(Square.at(i, j));
-        }
-        console.log(moves)
-        }
-        return moves;
+
+            //Left Diagonal Lower part
+            for (var i =1, j=location.row, k=location.col ; i <= Math.min(location.row,location.col); i++)
+            moves.push(Square.at(j-i, k-i)); 
+
+            //Left Diagonal Upper part
+            for (j=location.row+1, k=location.col+1 ; Math.max(j,k) <= MAX_INDEX; j++,k++)
+            moves.push(Square.at(j, k));
+
+            // Right Diagonal Lower part
+            for (var j=location.row-1, k=location.col+1 ; Math.max(j,k) <= MAX_INDEX && j >=0; j--,k++)
+            moves.push(Square.at(j, k));
+
+            // Right Diagonal Upper part
+            for (j=location.row+1, k=location.col-1 ; Math.max(j,k) <= MAX_INDEX && k >=0; j++,k--)
+            moves.push(Square.at(j, k));
+
+            return moves;
+        } 
     }
 }
+    
--- tests/engine/pieces/bishop.spec.js
+++ tests/engine/pieces/bishop.spec.js
@@ -8,7 +8,7 @@ describe('Bishop', () => {
     let board;
     beforeEach(() => board = new Board());
 
-    it('can move diagonally', () => {
+    it('can move diagonally 1', () => {
         const bishop = new Bishop(Player.WHITE);
         board.setPiece(Square.at(2, 4), bishop);
 
@@ -24,7 +24,7 @@ describe('Bishop', () => {
         moves.should.deep.include.members(expectedMoves);
     });
 
-    it('can move diagonally2', () => {
+    it('can move diagonally 2', () => {
         const bishop = new Bishop(Player.WHITE);
         board.setPiece(Square.at(1, 2), bishop);
 
@@ -48,4 +48,149 @@ describe('Bishop', () => {
 
         moves.should.have.length(11);
     });
+
+    it('can move diagonally 3', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(1, 2), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+
+        const expectedMoves = [
+            // Diagonally Right
+            Square.at(0, 1), Square.at(2, 3), Square.at(3, 4), Square.at(4, 5), Square.at(5, 6),Square.at(6, 7),
+            // Diagonally Left
+            Square.at(0, 3), Square.at(2, 1), Square.at(3, 0)
+        ];
+
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('cannot make any other moves2', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(1,2), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+
+        moves.should.have.length(9);
+    });
+
+    it('can move diagonally 4', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(4, 6), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+        
+
+        const expectedMoves = [
+            //right Diagonal
+            Square.at(0, 2), Square.at(1, 3), Square.at(2, 4), Square.at(3, 5), Square.at(5, 7),
+            // left Diagonal
+            Square.at(3, 7), Square.at(5, 5), Square.at(6, 4), Square.at(7,3)
+        ];
+        moves.should.have.length(9);
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('can move diagonally 5', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(4, 4), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+        
+
+        const expectedMoves = [
+            //right Diagonal
+            Square.at(3, 3), Square.at(2, 2), Square.at(1, 1), Square.at(0, 0), Square.at(5, 5), Square.at(6, 6), Square.at(7, 7),
+            // left Diagonal
+            Square.at(1, 7), Square.at(2, 6), Square.at(3, 5), Square.at(5, 3), Square.at(6, 2), Square.at(7, 1)
+        ];
+        moves.should.have.length(13);
+        //moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('can move diagonally 6', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(7, 2), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+        
+
+        const expectedMoves = [
+            //right Diagonal
+            Square.at(5, 0), Square.at(6, 1),
+            // left Diagonal
+            Square.at(2, 7), Square.at(3, 6), Square.at(4, 5), Square.at(5, 4), Square.at(6, 3)
+        ];
+        moves.should.have.length(7);
+        //moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('can move diagonally 7', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(7, 7), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+        
+
+        const expectedMoves = [
+            //right Diagonal
+            Square.at(6, 6), Square.at(5, 5), Square.at(4, 4), Square.at(3, 3), Square.at(2, 2), Square.at(1, 1), Square.at(0, 0)
+            // left Diagonal
+
+        ];
+        moves.should.have.length(7);
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('can move diagonally 8', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(0, 0), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+    
+
+        const expectedMoves = [
+            //right Diagonal
+            Square.at(1, 1), Square.at(2, 2), Square.at(3, 3), Square.at(4, 4), Square.at(5, 5), Square.at(6, 6), Square.at(7, 7)
+            // left Diagonal
+
+        ];
+        moves.should.have.length(7);
+        moves.should.deep.include.members(expectedMoves);
+    });
+it('can move diagonally 9', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(0, 7), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+        
+
+        const expectedMoves = [
+            //right Diagonal
+            Square.at(1, 6), Square.at(2, 5), Square.at(3, 4), Square.at(4, 3), Square.at(5, 2), Square.at(6, 1), Square.at(7, 0)
+            // left Diagonal
+
+        ];
+        moves.should.have.length(7);
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    it('can move diagonally 10', () => {
+        const bishop = new Bishop(Player.WHITE);
+        board.setPiece(Square.at(7, 0), bishop);
+
+        const moves = bishop.getAvailableMoves(board);
+        
+
+        const expectedMoves = [
+            //right Diagonal
+            Square.at(0, 7), Square.at(1, 6), Square.at(2, 5), Square.at(3, 4), Square.at(4, 3), Square.at(5, 2), Square.at(6, 1)
+            // left Diagonal
+
+        ];
+        moves.should.have.length(7);
+        moves.should.deep.include.members(expectedMoves);
+    });
+
+    
 });
